1923bacd0cb9b0b9362159074e9cd2aeebe3b269
Payam <payam.broumandi@gmail.com>
2021-04-08 16:51:20 +0430

    initla repository project

---
+++ GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+    {
+        public string Add(T entity)
+        {
+            return entity.ToString();
+        }
+
+        public IEnumerable<T> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class Person
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string FullName() => $"{Fname} {Lname}";
+
+        public override string ToString() => $"{Fname} {Lname}";
+    }
+}
